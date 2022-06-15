07988d924cdad27a82714a3ac446afdae6af59f7
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-15 09:17:18 +0300

    feat: сверстать страницу 404

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,9 +1,18 @@
+import { useHistory } from 'react-router-dom'
 import './NotFound.css';
 
 function Login() {
+  const history = useHistory()
+
+  function handleClick() {
+    history.goBack();
+  }
+
   return (
     <div className="page-404">
-      NotFound
+      <p className="page-404__title">404</p>
+      <p className="page-404__subtitle">Страница не найдена</p>
+      <button className="page-404__back-button" onClick={handleClick}>Назад</button>
     </div>
   );
 }
