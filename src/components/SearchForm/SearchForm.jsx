c8553d1a595c40415c05635dec9f25b424faa5a5
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-16 00:54:36 +0300

    feat: верстка секции поиска фильмов

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,33 @@
 import './SearchForm.css';
 
-function SearchForm() {
+function SearchForm({ isShort, setIsShort, searchQuery, setSearchQuery, searchFilms }) {
   return (
-    <form className="search-form">
-
-    </form>
+    <section className="search">
+      <div className="container search__container">
+        <form className="search__form from-search" onSubmit={searchFilms}>
+          <input
+            className="from-search__input"
+            type="text"
+            placeholder='Фильм'
+            value={searchQuery}
+            onInput={e => setSearchQuery(e.target.value)}
+          />
+          <button className="from-search__button"></button>
+          <label className="from-search__label" >
+            <input
+              className="from-search__checkbox"
+              type="checkbox"
+              checked={isShort}
+              onChange={() => setIsShort(!isShort)}
+            />
+            <div className="from-search__custom-checkbox">
+              <div className="from-search__custom-mark"></div>
+            </div>
+            <p className="form-search__label-text">Короткометражки</p>
+          </label>
+        </form>
+      </div>
+    </section>
   );
 }
 
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import './Movies.css';
+import SearchForm from '../../components/SearchForm/SearchForm';
 
 function Movies() {
+  const [isShort, setIsShort] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const searchFilms = (evt) => {
+    evt.preventDefault()
+    console.log(isShort, searchQuery);
+  }
+
   return (
     <div className="movies-page">
+      <SearchForm
+        isShort={isShort}
+        searchQuery={searchQuery}
+        setIsShort={setIsShort}
+        setSearchQuery={setSearchQuery}
+        searchFilms={searchFilms}
+      />
       Movies
     </div>
   );
