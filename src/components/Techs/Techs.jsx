22ce2bc3b08345ed225048d04c35bce4c507f76e
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-15 06:42:44 +0300

    feat: верстка блока технологии

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -3,7 +3,20 @@ import './Techs.css';
 function Techs() {
   return (
     <section className="techs">
-
+      <div className="container">
+        <h3 className="title techs__title">Технологии</h3>
+        <p className="techs__subtitle">7 технологий</p>
+        <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <ul className="techs__list">
+          <li className="techs__item">HTML</li>
+          <li className="techs__item">CSS</li>
+          <li className="techs__item">JS</li>
+          <li className="techs__item">React</li>
+          <li className="techs__item">Git</li>
+          <li className="techs__item">Express.js</li>
+          <li className="techs__item">mongoDB</li>
+        </ul>
+      </div>
     </section>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import './Main.css';
 import AboutProject from '../../components/AboutProject/AboutProject'
+import Techs from '../../components/Techs/Techs'
 
 function Main() {
   return (
     <div className="main-page">
       Main
       <AboutProject />
+      <Techs />
     </div>
   );
 }
