1d906b00a4f11493baaf185aa20a8ee4cd906f39
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-15 16:05:01 +0300

    сделать хедер при выполненом входе

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,38 @@
-import { Link } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import './Header.css';
 import Logo from '../Logo/Logo'
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
+  const currentPath = useHistory().location.pathname
+  const isMainPage = currentPath === '/'
+  const isLoggedIn = true
+
+  const buttons = isLoggedIn ? (
+    <div className="header__profile">
+      <Link className="header__profile-button" to='/profile'>Аккаунт</Link>
+      <div className="header__profile-photo"></div>
+    </div>
+  ) : (
+    <div className="header__buttons">
+      <Link className="header__button" to='/signup'>Регистрация</Link>
+      <Link className="header__button header__button_color_green" to='/signin'>Войти</Link>
+    </div>
+  )
+
   return (
-    <header className="header">
+    <header
+      className="header"
+      style={{ backgroundColor: isMainPage && '#073042' }}
+    >
       <div className="container header__container">
         <div className="header__wrapper">
           <Logo />
-          <div className="header__buttons">
-            <Link className="header__button" to='/signup'>Регистрация</Link>
-            <Link className="header__button header__button_color_green" to='/signin'>Войти</Link>
-          </div>
+          {isLoggedIn && <Navigation />}
+          {buttons}
         </div>
       </div>
-    </header>
+    </header >
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,21 @@
+import { NavLink } from "react-router-dom";
 import './Navigation.css';
 
 function Navigation() {
   return (
     <nav className="nav">
-
+      <ul className="nav__list">
+        <li><NavLink
+          className="nav__link"
+          activeClassName="nav__link_active"
+          to="/movies"
+        >Фильмы</NavLink></li>
+        <li><NavLink
+          className="nav__link"
+          activeClassName="nav__link_active"
+          to="/saved-movies"
+        >Сохранённые фильмы</NavLink></li>
+      </ul>
     </nav>
   );
 }
