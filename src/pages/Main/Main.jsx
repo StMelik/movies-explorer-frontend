250c008ab10aa147a518b1db662015161d24c184
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-15 09:02:50 +0300

    верстка промо блока

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,5 @@
 import './Main.css';
+import Promo from '../../components/Promo/Promo'
 import NavTab from '../../components/NavTab/NavTab'
 import AboutProject from '../../components/AboutProject/AboutProject'
 import Techs from '../../components/Techs/Techs'
@@ -8,6 +9,7 @@ import Footer from '../../components/Footer/Footer'
 function Main() {
   return (
     <div className="main-page">
+      <Promo />
       <NavTab />
       <AboutProject />
       <Techs />
