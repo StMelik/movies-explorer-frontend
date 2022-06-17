9a58861737fe350b09ac775d0ec495222f431b1f
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-17 13:50:31 +0300

    feat: адаптив блока портфолио

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,9 +5,15 @@ function Portfolio() {
     <section className="portfolio">
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__list">
-        <li className="portfolio__item"><a className="portfolio__link" href="https://stmelik.github.io/how-to-learn/" target="_blank">Статичный сайт</a></li>
-        <li className="portfolio__item"><a className="portfolio__link" href="https://stmelik.github.io/russian-travel/" target="_blank">Адаптивный сайт</a></li>
-        <li className="portfolio__item"><a className="portfolio__link" href="https://stmelik.store/" target="_blank">Одностраничное приложение</a></li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.github.io/how-to-learn/" target="_blank">Статичный сайт</a>
+        </li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.github.io/russian-travel/" target="_blank">Адаптивный сайт</a>
+        </li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.store/" target="_blank">Одностраничное приложение</a>
+        </li>
       </ul>
     </section>
   );
