e921e2925efa3b6d0bda32081d36cc9369e0493d
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-17 06:49:58 +0300

    fix: исправить отображенние 404 страницы на мобиле

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -4,14 +4,19 @@ import './NotFound.css';
 function Login() {
   const history = useHistory()
 
+  // исправляет высоту на мобилке
+  const h = window.innerHeight + 'px'
+
   function handleClick() {
     history.goBack();
   }
 
   return (
-    <div className="page-404">
-      <p className="page-404__title">404</p>
-      <p className="page-404__subtitle">Страница не найдена</p>
+    <div className="page-404" style={{ height: h }}>
+      <div className="page-404__text">
+        <p className="page-404__title">404</p>
+        <p className="page-404__subtitle">Страница не найдена</p>
+      </div>
       <button className="page-404__back-button" onClick={handleClick}>Назад</button>
     </div>
   );
