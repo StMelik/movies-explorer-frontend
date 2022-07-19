fc9aad961239a4ca7496f90810617a08769b141f
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-07-20 01:46:55 +0300

    update: обновить портфолио

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,13 +6,22 @@ function Portfolio() {
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <a className="portfolio__link" href="https://stmelik.github.io/how-to-learn/" target="_blank">Статичный сайт</a>
+          <a className="portfolio__link" href="https://stmelik.store/" target="_blank">Одностраничное приложение</a>
         </li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.github.io/vue-todo-app/" target="_blank">Список задач</a>
+        </li>
+
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.github.io/vue-pokemon-lib/" target="_blank">Библиотека покемонов</a>
+        </li>
+
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="https://stmelik.github.io/online-cinema/" target="_blank">Онлайн Кинотеатр</a>
+        </li>
+
         <li className="portfolio__item">
           <a className="portfolio__link" href="https://stmelik.github.io/russian-travel/" target="_blank">Адаптивный сайт</a>
-        </li>
-        <li className="portfolio__item">
-          <a className="portfolio__link" href="https://stmelik.store/" target="_blank">Одностраничное приложение</a>
         </li>
       </ul>
     </section>
